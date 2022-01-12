eb4b6d33f03e1f204f92ba2172a117e3d8dda679
Tenka5 <vteremetskiy@gmail.com>
2022-01-12 03:33:30 +0300

    first commit

---
+++ lab2.3/app/src/main/java/ru/samsung/itacademy/mdev/manylayout/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.samsung.itacademy.mdev.manylayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
